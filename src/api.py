f8db4faab075c1c34c33c23531750c1fd73f9605
Sneha Mungre <snehamungre@gmail.com>
2026-02-25 14:00:14 +0400

    setting up project and API connection

---
+++ src/api.py
@@ -0,0 +1,36 @@
+import datetime
+import json
+import requests
+
+# TODO: set up test for API connection
+# TODO: add error message for when the data doesn't load
+
+def get_data():
+    # Test API endpoint
+    try:
+        test = "https://api.coingecko.com/api/v3/ping?"
+        r = requests.get(test)
+
+    except requests.exceptions.RequestException as e:  # This is the correct syntax
+        raise SystemExit(e)
+
+    # Define API endpoint
+    url = (
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&sparkline=true"
+    )
+
+    headers = {"x_cg_demo_api_key": "CG - G9RLuK6SZfA44AL3DPwnFLKT"}
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+
+        file_path = "data/crypto_market_data_raw.json"
+
+        with open(file_path, "a", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+        print("data saved in file")
+
+    else:
+        print(f"Error retrieving data, status code: {response.status_code}")
---
+++ src/main.py
@@ -0,0 +1,5 @@
+from api import get_data
+
+
+if __name__ == "__main__":
+    get_data()
