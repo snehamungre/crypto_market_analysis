20c6489e5c4f58f1db7d3f9d896475a22f8a366d
Sneha Mungre <snehamungre@gmail.com>
2026-03-05 14:04:46 +0400

    adding a schedule to fetch data

--- src/api.py
+++ src/api.py
@@ -13,12 +13,7 @@ def get_data():
 
     except requests.exceptions.RequestException as e:  # This is the correct syntax
         raise SystemExit(e)
-    
-    get_market_data()
-    get_historical_data()
 
-
-def get_historical_data():   
     # Define API endpoint
     url = (
         "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&sparkline=true"
@@ -32,35 +27,7 @@ def get_historical_data():
 
         timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
         # store API response to raw file
-        file_path = f"data/raw/crypto_historical_data_raw_{timestamp}.json"
-
-        with open(file_path, "w", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
-
-        # todo remove this print line
-        print("data saved in file")
-
-    else:
-        print(f"Error retrieving data, status code: {response.status_code}")
-
-
-def get_market_data():
-    # Define API endpoint
-    url = (
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&sparkline=true"
-    )
-
-    headers = {"x_cg_demo_api_key": "CG - G9RLuK6SZfA44AL3DPwnFLKT"}
-
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-
-        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-        # store API response to raw file
-        file_path = (
-            f"data/raw/crypto_market_data_raw_{timestamp}.json"
-        )
+        file_path = f"data/raw/crypto_market_data_raw_{timestamp}.json"
 
         with open(file_path, "w", encoding="utf-8") as f:
             json.dump(data, f, ensure_ascii=False, indent=4)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,13 @@
 from api import get_data
+import schedule
+import time
 
 
 if __name__ == "__main__":
-    get_data()
+    # Simple, human-readable scheduling
+    schedule.every(24).hours.do(get_data)
+    schedule.every().day.at("08:30").do(get_data)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
